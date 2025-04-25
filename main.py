a456172269205c3f6f84b2787a5d4f2d7ac15729
MagyoDev <magyodev@gmail.com>
2025-04-25 14:46:00 -0300

    v0.5 - Adiciona Hitsugaya com Daiguren Hyorinmaru

--- main.py
+++ main.py
@@ -13,9 +13,16 @@ class Byakuya(Personagem):
     def bankai(self):
         print(f"{self.nome} ativa Senbonzakura Kageyoshi! Milhares de lâminas voam em direção ao inimigo!")
 
+class Hitsugaya(Personagem):
+    def bankai(self):
+        print(f"{self.nome} ativa Daiguren Hyorinmaru! O inimigo começa a ser congelado!")
+
 if __name__ == "__main__":
     ichigo = Ichigo("Ichigo Kurosaki")
     ichigo.bankai()
 
     byakuya = Byakuya("Byakuya Kuchiki")
     byakuya.bankai()
+
+    hitsugaya = Hitsugaya("Tōshirō Hitsugaya")
+    hitsugaya.bankai()
