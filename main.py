9539ba2d913815caf89b889e32ee1af0aa45b154
MagyoDev <magyodev@gmail.com>
2025-04-25 14:45:02 -0300

    v0.4 - Adiciona Byakuya com Senbonzakura Kageyoshi

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ class Ichigo(Personagem):
     def bankai(self):
         print(f"{self.nome} ativa Tensa Zangetsu! Velocidade e ataque aumentados drasticamente!")
 
+class Byakuya(Personagem):
+    def bankai(self):
+        print(f"{self.nome} ativa Senbonzakura Kageyoshi! Milhares de lâminas voam em direção ao inimigo!")
+
 if __name__ == "__main__":
     ichigo = Ichigo("Ichigo Kurosaki")
     ichigo.bankai()
+
+    byakuya = Byakuya("Byakuya Kuchiki")
+    byakuya.bankai()
